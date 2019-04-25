81729272b1a998f8dea213531b65d3a26aa7c11d
Wenhb <whbjzzwjxq@gmail.com>
2019-04-25 08:24:11 +0800

    Null file remove

--- document/document.py
+++
@@ -1,8 +0,0 @@
-from document import models
-
-# 新增专题
-def add(filedata = {}):
-    doc = models.Document.objects.create(**filedata)
-    return doc
-
-
--- newcontent/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewcontentConfig(AppConfig):
-    name = 'newcontent'
