54132b5f42b1b329ff34876cad8a89f00856a1d8
Wenhb <whbjzzwjxq@gmail.com>
2019-05-06 14:20:44 +0800

    Update es_search.py

--- search/es_search.py
+++ search/es_search.py
@@ -38,34 +38,40 @@ def complete_ask(keyword, index):
 def fuzzy_ask_node(request):
     if request.method == 'GET':
         keyword = request.GET.get('keyword')
-        results = fuzzy_ask(keyword, 'name.keyword', 'nodes-strnode')
-        results_doc = fuzzy_ask(keyword, 'name.keyword', 'nodes-document')
-        uuids = []
-        # 通过符合的节点搜索专题
-        if results:
-            results = results['hits']['hits']
-            for result in results:
-                for node in search_doc_by_node(search_by_uuid(result['_id'])):
-                    uuids.append(node['uuid'])
-                # todo 调整一下索引 这里两次搜索太蠢了
-        # 直接搜索符合的专题
-        if results_doc:
-            results_doc = results_doc['hits']['hits']
-            #todo 这里没有排序 以后要改一下
-            for x in results_doc:
-                uuids.append(x['_id'])
-        result = views.get_cache_doc(uuids)
-        return HttpResponse(json.dumps(result, ensure_ascii=False))
+        if keyword:
+            results = fuzzy_ask(keyword, 'name.keyword', 'nodes-strnode')
+            results_doc = fuzzy_ask(keyword, 'name.keyword', 'nodes-document')
+            uuids = []
+            # 通过符合的节点搜索专题
+            if results:
+                results = results['hits']['hits']
+                for result in results:
+                    for node in search_doc_by_node(search_by_uuid(result['_id'])):
+                        uuids.append(node['uuid'])
+                    # todo 调整一下索引 这里两次搜索太蠢了
+            # 直接搜索符合的专题
+            if results_doc:
+                results_doc = results_doc['hits']['hits']
+                # todo 这里没有排序 以后要改一下
+                for x in results_doc:
+                    uuids.append(x['_id'])
+            result = views.get_cache_doc(uuids)
+            return HttpResponse(json.dumps(result, ensure_ascii=False))
+        else:
+            return HttpResponse(json.dumps([], ensure_ascii=False))
 
 
 def auto_complete(request):
     if request.method == 'GET':
         keyword = request.GET.get('keyword')
-        results_doc = complete_ask(keyword, 'nodes-document')
-        uuids = []
-        if results_doc:
-            results_doc = results_doc['hits']['hits']
-            for x in results_doc:
-                uuids.append(x['_id'])
-        result = views.get_cache_doc(uuids)
-        return HttpResponse(json.dumps(result, ensure_ascii=False))
+        if keyword:
+            results_doc = complete_ask(keyword, 'nodes-document')
+            uuids = []
+            if results_doc:
+                results_doc = results_doc['hits']['hits']
+                for x in results_doc:
+                    uuids.append(x['_id'])
+            result = views.get_cache_doc(uuids)
+            return HttpResponse(json.dumps(result, ensure_ascii=False))
+        else:
+            return HttpResponse(json.dumps([], ensure_ascii=False))
