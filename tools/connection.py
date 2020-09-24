3f0351789be99b807bb9c1705f5796084a240dfc
whbjzzwjxq <whbjzzwjxq@gmail.com>
2020-09-24 23:27:49 +0800

    update

--- tools/connection.py
+++ tools/connection.py
@@ -1,7 +1,9 @@
+import os
 import redis
 from elasticsearch import Elasticsearch
 from py2neo import Graph
 
+support_host = os.getenv('SUPPORT_HOST', '127.0.0.1')
 # postgre
 DATABASES = {
     "default": {
@@ -9,18 +11,18 @@ DATABASES = {
         "NAME": "metaknew_production",
         "USER": "metaknew",
         "PASSWORD": "12345678",
-        "HOST": "101.200.192.71",
+        "HOST": support_host,
         "PORT": "5432",
     }
 }
 
 # redis
-pool = redis.ConnectionPool(host="101.200.192.71", port=6379, db=1, decode_responses=True)
+pool = redis.ConnectionPool(host=support_host, port=6379, db=1, decode_responses=True)
 redis_instance = redis.StrictRedis(connection_pool=pool)
 
 # Neo4j
-graph = Graph("bolt://101.200.192.71:7687", Name="neo4j", password="12345678")
+graph = Graph(f"bolt://{support_host}:7687", Name="neo4j", password="12345678")
 
 
 # elasticsearch
-es_connection = Elasticsearch([{"host": "101.200.192.71", "port": 8013}])
+es_connection = Elasticsearch([{"host": support_host, "port": 8013}])
