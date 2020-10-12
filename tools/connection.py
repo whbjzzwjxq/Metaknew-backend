394c5eb0519c4a02a2aeac50be393f80ddb38a93
whbjzzwjxq <whbjzzwjxq@gmail.com>
2020-10-13 05:44:14 +0800

    [Change] Connection

--- tools/connection.py
+++ tools/connection.py
@@ -3,26 +3,29 @@ import redis
 from elasticsearch import Elasticsearch
 from py2neo import Graph
 
-gateway = os.getenv('GATEWAY', '127.0.0.1')
-# postgre
+postgre_container_name = os.getenv("POSTGRE_NAME", "postgre_container")
+redis_container_name = os.getenv("REDIS_NAME", "redis_container")
+neo4j_container_name = os.getenv("NEO4J_NAME", "neo4j_container")
+es_container_name = os.getenv("ES_NAME", "es_container")
+
+# postgresql
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "metaknew_production",
         "USER": "metaknew",
         "PASSWORD": "12345678",
-        "HOST": gateway,
+        "HOST": postgre_container_name,
         "PORT": "5432",
     }
 }
 
 # redis
-pool = redis.ConnectionPool(host=gateway, port=6379, db=1, decode_responses=True)
+pool = redis.ConnectionPool(host=redis_container_name, port=6379, db=1, decode_responses=True)
 redis_instance = redis.StrictRedis(connection_pool=pool)
 
 # Neo4j
-graph = Graph(f"bolt://{gateway}:7687", Name="neo4j", password="12345678")
-
+graph = Graph(f"bolt://{neo4j_container_name}:7687", Name="neo4j", password="12345678")
 
 # elasticsearch
-es_connection = Elasticsearch([{"host": gateway, "port": 8013}])
+es_connection = Elasticsearch([{"host": es_container_name, "port": 9200}])
